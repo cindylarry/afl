e207d0e6efae2b5eec59f1f48acc667f82c7ff01
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2017-04-17 02:28:34 -0700

    [klee] llvm_mode: add minimal runtime

---
+++ llvm_mode/afl-klee-llvm-rt.o.c
@@ -0,0 +1,31 @@
+/*
+   american fuzzy lop - LLVM instrumentation bootstrap
+   ---------------------------------------------------
+
+   Written by Laszlo Szekeres <lszekeres@google.com> and
+              Michal Zalewski <lcamtuf@google.com>
+
+   LLVM integration design comes from Laszlo Szekeres.
+
+   Copyright 2015, 2016 Google Inc. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at:
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   This code is a minimal runtime designed to be used with KLEE.
+
+*/
+
+#include "../config.h"
+#include "../types.h"
+
+/* Globals needed by the injected instrumentation. The __afl_area_initial region
+   is used for instrumentation output before __afl_map_shm() has a chance to run.
+   It will end up as .comm, so it shouldn't be too wasteful. */
+
+u8  __afl_area_initial[MAP_SIZE];
+u8* __afl_area_ptr = __afl_area_initial;
+u32 __afl_prev_loc;
